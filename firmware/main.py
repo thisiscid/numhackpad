188192c41d26742de2d9270fa9d0fe1d49edcf25
Cid <137782638+thisiscid@users.noreply.github.com.>
2025-07-06 13:40:13 -0400

    final update, just a main.py thing where i moved it round (also, changed up the pin order bcs i accidentally did it wrong)

---
+++ firmware/main.py
@@ -0,0 +1,36 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner, DiodeOrientation
+from kmk.keys import KC
+from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.scanners import DiodeOrientation
+
+keyboard = KMKKeyboard()
+macros = Macros()
+keyboard.modules.append(macros)
+keyboard.row_pins = (board.GP27, board.GP28, board.GP29, board.GP6, board.GP7)
+keyboard.col_pins = (board.GP3, board.GP4, board.GP2, board.GP1)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.keymap = [
+    [
+        # row 1
+        KC.NLCK, KC.PSLS, KC.PAST, KC.PMNS,
+
+        # row 2
+        KC.P7, KC.P8, KC.P9, KC.PPLS, 
+
+        # row 3
+        KC.P4, KC.P5, KC.P6, KC.PDOT,
+
+        # row 4
+        KC.P1, KC.P2, KC.P3, KC.PENT,
+        
+        # row 5
+        KC.P0, KC.NO, KC.NO, KC.NO
+    ]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
